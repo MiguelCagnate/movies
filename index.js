a3848992cd3694d92beb424ebe71ca5b74482c74
MiguelCagnate <omikeflies@gmail.com>
2022-06-27 18:23:16 +0200

    commit1

---
+++ index.js
@@ -0,0 +1,34 @@
+import { films } from "./films.js";
+//console.log(films);
+
+
+// show all films titles
+function showTitleFilms() {
+let showAllTitles = films.filter((t) => t.Title)
+console.log(showTitleFilms);
+return showAllTitles; 
+}
+
+showTitleFilms();
+
+// create new array with imdbRating to number
+function changeRatingToNumber() {
+
+}
+// order films by year
+function orderFilmsByYear() {}
+orderFilmsByYear();
+
+// show comming soon films
+function showCommingSoonFilms() {}
+showCommingSoonFilms();
+
+// find some serie with genere Action
+function findSomeSerie() {}
+findSomeSerie();
+
+// reduce imdbRating and calculate average
+function averageRating() {
+
+}
+averageRating();
